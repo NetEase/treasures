3f4dc2622c21fd742fb953e8e22a0ad1791be292
Johnny Wong <wzhao23@gmail.com>
2012-11-16 10:42:46 +0800

    update pomelo version

--- game-server/app/servers/connector/handler/entryHandler.js
+++ game-server/app/servers/connector/handler/entryHandler.js
@@ -26,11 +26,12 @@ pro.entry = function(msg, session, next) {
   var self = this;
   var playerId = parseInt(this.serverId + id, 10);
   id += 1;
-  session.bind(playerId, function(){});
+  session.bind(playerId);
   session.set('playerId', playerId);
   //session.set('playername', msg.name);
   session.set('areaId', 1);
   session.on('closed', onUserLeave.bind(null, self.app));
+  session.pushAll();
   next(null, {code: Code.OK, playerId: playerId});
 };
 
