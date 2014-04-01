eed2f565e4d27b2d76048bcc9ab5ff2d2d7edc5c
wei.wu <dlutwuwei@users.noreply.github.com>
2014-04-01 13:24:15 +0800

    负载均衡的代码bug
    crc32返回的是十六进制数据，需要转化为十进制。

--- game-server/app/util/dispatcher.js
+++ game-server/app/util/dispatcher.js
@@ -1,7 +1,7 @@
 var crc = require('crc');
 
 module.exports.dispatch = function(uid, connectors) {
-	var index = Math.abs(crc.crc32(uid)) % connectors.length;
+	var index = Math.abs(parseInt(crc.crc32(uid)),16) % connectors.length;
 	return connectors[index];
 };
 
